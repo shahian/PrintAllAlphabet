dcbb0699738ab3431a9e43bb924d2c22b825e39f
h.shahian <h.shahian@haytech.ir>
2022-10-15 11:39:16 +0330

    Initial commit

---
+++ src/com/shahian/Main.java
@@ -0,0 +1,11 @@
+package com.shahian;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char ch;
+        for (ch = 'a'; ch <= 'z'; ch++) {
+            System.out.println(ch);
+        }
+    }
+}
